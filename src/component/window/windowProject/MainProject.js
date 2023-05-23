a0ccf892c85cdc001ff61e1a42d1302e081f6299
thseogns <99688960+thseogns@users.noreply.github.com>
2023-05-23 14:57:57 +0900

    Update MainProject.js

--- src/component/window/windowProject/MainProject.js
+++ src/component/window/windowProject/MainProject.js
@@ -2,11 +2,13 @@
 
 import React from "react";
 import SimpleSlider from "./SimpleSlider";
-
+import HoverPage from "./HoverPage";
+import styles from "./MainProject.module.css";
 const MainProject = () => {
   return (
-    <div>
+  <div className={styles.margin}>
       <SimpleSlider />
+    <HoverPage />
     </div>
   );
 };
